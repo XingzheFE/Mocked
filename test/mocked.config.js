297a3011912786a65f4396f9c58e47c561c7228a
joe223 <heyin223@163.com>
2017-06-02 00:34:11 +0800

    add: config

--- test/mocked.config.js
+++ test/mocked.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    
+    port: 4443,                         
+    enableWrite: false,                 // post/patch/delete method won't change db.json
+    database: "./db.json",              // response data
+    route: "./routes.json",             // route list
 }
