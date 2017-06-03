e92e19ad63f05fc47c4c56a3ada3cd100a8521ac
joe223 <heyin223@163.com>
2017-06-03 15:37:06 +0800

    what we are going to do

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mocked-js
+a light mock server for developer.
+
+- random data
+- repeat
+- writable
+- RESTful API
--- bin/mocked.js
+++ bin/mocked.js
@@ -1,3 +1,27 @@
-var program = require('commander')
-    program
+const loader = require('../lib/loader.js');
+
+let program = require('commander');
+let path = require('path');
+let mocked = require('../lib/server.js');
+let customConf = loader('./mocked.config.js');
+let defaultConf = require("../lib/default.config.js");
+
+Object.assign(defaultConf, customConf);
+
+program
     .version(require('../package').version)
+    .option('-c, --config <n>', 'Mocked configuration')
+    .option('-p, --port <n>', 'An integer argument', parseInt)
+    .option('-r, --route <n>', 'Route json file path')
+    .option('-d, --data <n>', 'Database json file path')
+    .parse(process.argv);
+
+let serverPort = program.port;
+let routesPath = program.route ? path.resolve(process.cwd(), program.route) : '';
+let databasePath = program.data ? path.resolve(process.cwd(), program.data) : '';
+
+console.log(serverPort);
+console.log(routesPath);
+console.log(databasePath);
+
+mocked(defaultConf);
